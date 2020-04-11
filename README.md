d0fbb2965e05ceb7b3efe4624f5beb4c61c2b0d2
davidalejandrolazopampa <40555746+davidalejandrolazopampa@users.noreply.github.com>
2020-04-11 16:25:01 -0500

    Iniciando

---
+++ README.md
@@ -0,0 +1,7 @@
+# Trabajo
+Uva - 468 "Key to success 468"
+## Alumno
+David Lazo - 201810010
+```
+Proceso
+```
---
+++ main.cpp
@@ -0,0 +1,9 @@
+//David Lazo 201810010
+//UVA 468
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   
+}
